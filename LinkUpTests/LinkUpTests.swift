82910cbb3476eda8ed7a42c8b791f58de68b20e3
212593046 <212593046@ge.com>
2017-08-14 13:20:53 -0400

    Initial Commit

---
+++ LinkUpTests/LinkUpTests.swift
@@ -0,0 +1,36 @@
+//
+//  LinkUpTests.swift
+//  LinkUpTests
+//
+//  Created by Del Shawn Kirksey on 8/14/17.
+//  Copyright © 2017 Del Shawn Kirksey. All rights reserved.
+//
+
+import XCTest
+@testable import LinkUp
+
+class LinkUpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
